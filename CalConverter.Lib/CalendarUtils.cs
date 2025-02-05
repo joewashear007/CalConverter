3515f65feda051f001124285073f62d612baaa39
Joseph Livecchi <jlivecchi@servicetitan.com>
2025-02-05 13:43:36 -0500

    Added summarize file

---
+++ CalConverter.Lib/CalendarUtils.cs
@@ -0,0 +1,41 @@
+﻿using Ical.Net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalConverter.Lib;
+public static class CalendarUtils
+{
+    public static string Summarize(Calendar calendar)
+    {
+        var events = calendar.Events
+            .GroupBy(q => q.Start.Date)
+            .OrderBy(q => q.Key)
+            .ToList();
+
+
+        StringBuilder summary = new StringBuilder();
+        foreach (var dayGroup in events)
+        {
+            summary
+                .AppendLine(dayGroup.Key.ToShortDateString())
+                .AppendLine("---------------------------");
+            var dayEvents = dayGroup.OrderByDescending(q => q.Start.Hour + q.Attendees.FirstOrDefault()?.CommonName ?? "").ToList();
+            foreach (var dayEvent in dayEvents)
+            {
+                summary.Append(" * ").AppendFormat("{0:00}", dayEvent.Start.Hour).Append(":00 - ").Append(dayEvent.Summary);
+                if (dayEvent.Resources.Count > 0)
+                {
+                    summary.Append(" ( Room: ").Append(dayEvent.Resources.First()).Append(")");
+                }
+                summary.AppendLine();
+            }
+            summary.AppendLine().AppendLine();
+        }
+
+        return summary.ToString();
+    }
+}
